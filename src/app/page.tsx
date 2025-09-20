5772cf5ae091fe99623dc91ab414da9f7cb0aa25
Alexander Mitrofanov <www.lexter70@gmail.com>
2025-09-20 19:00:19 +0300

    refactor Header component to include navigation, logo, and action buttons; update global styles with theme colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,48 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <ContainerPage>
-      <Header className={"flex items-center justify-between"}>
-        <nav>
-          <ul className={"flex w-109.5 items-center justify-between"}>
-            <li key={crypto.randomUUID()}>ЖЕНЩИНАМ</li>
-            <li key={crypto.randomUUID()}>МУЖЧИНАМ</li>
-            <li key={crypto.randomUUID()}>АКСЕССУАРЫ</li>
-          </ul>
-        </nav>
-        <div>
-          <Image
-            src={"/images/logo.svg"}
-            alt={"Logo"}
-            width={115}
-            height={31}
-            priority={true}
-          />
-        </div>
-        <div className={"flex w-74 items-center justify-between"}>
-          <button>О БРЕНДЕ</button>
-          <Image
-            src={"/images/search.svg"}
-            alt={"Search"}
-            width={24}
-            height={24}
-            priority={true}
-          />
-          <Image
-            src={"/images/user.svg"}
-            alt={"Search"}
-            width={24}
-            height={24}
-            priority={true}
-          />
-          <Image
-            src={"/images/bag.svg"}
-            alt={"Search"}
-            width={24}
-            height={24}
-            priority={true}
-          />
-        </div>
-      </Header>
+      <Header />
       <Main>Main</Main>
       <Footer>Footer</Footer>
     </ContainerPage>
--- src/widgets/header.tsx
+++ src/widgets/header.tsx
@@ -1,12 +1,288 @@
-import { FC, ReactNode } from "react";
+"use client";
 
-export const Header: FC<{ children: ReactNode; className?: string }> = ({
-  children,
-  className,
-}) => {
+import Image from "next/image";
+import { FC, useState } from "react";
+
+export const Header: FC<{ className?: string }> = ({ className }) => {
+  const [categoriesType, setCategoriesType] = useState<
+    "women" | "men" | "accessories" | null
+  >(null);
+
   return (
-    <header>
-      <section className={`px-13.5 py-3.5 ${className}`}>{children}</section>
+    <header className={"relative h-16"}>
+      <section
+        className={`flex h-full items-center justify-between px-13.5 py-2.5 ${className}`}
+      >
+        <HeaderNav setCategoriesType={setCategoriesType} />
+        <HeaderLogo />
+        <HeaderActions />
+      </section>
+      {categoriesType !== null && (
+        <HeaderCategories categoriesType={categoriesType} />
+      )}
     </header>
+  );
+};
+
+const HeaderNav: FC<{
+  setCategoriesType: (type: "women" | "men" | "accessories" | null) => void;
+}> = ({ setCategoriesType }) => {
+  return (
+    <nav>
+      <ul className={"flex w-2/5 items-center gap-8"}>
+        <li
+          key={crypto.randomUUID()}
+          onClick={() => setCategoriesType("women")}
+          className={
+            "hover:text-hover active:text-active cursor-pointer transition-colors"
+          }
+        >
+          ЖЕНЩИНАМ
+        </li>
+        <li
+          key={crypto.randomUUID()}
+          onClick={() => setCategoriesType("men")}
+          className={
+            "hover:text-hover active:text-active cursor-pointer transition-colors"
+          }
+        >
+          МУЖЧИНАМ
+        </li>
+        <li
+          key={crypto.randomUUID()}
+          onClick={() => setCategoriesType("accessories")}
+          className={
+            "hover:text-hover active:text-active cursor-pointer transition-colors"
+          }
+        >
+          АКСЕССУАРЫ
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+const HeaderLogo = () => {
+  return (
+    <div className={"flex w-1/5 items-center justify-center"}>
+      <Image
+        src={"/images/logo.svg"}
+        alt={"Logo"}
+        width={115}
+        height={31}
+        priority={true}
+      />
+    </div>
+  );
+};
+
+const HeaderActions = () => {
+  const [visibleSearch, setVisibleSearch] = useState<boolean>(false);
+
+  return (
+    <div className={"flex w-2/5 items-center justify-end gap-8"}>
+      {visibleSearch ? (
+        <input type={"text"} className={"h-11 w-86 border-1 border-zinc-950"} />
+      ) : (
+        <button
+          className={
+            "hover:text-hover active:text-active cursor-pointer transition-colors"
+          }
+        >
+          О БРЕНДЕ
+        </button>
+      )}
+
+      <Image
+        src={"/images/search.svg"}
+        alt={"Search"}
+        width={24}
+        height={24}
+        priority={true}
+        onClick={() => setVisibleSearch((visible) => !visible)}
+      />
+      <Image
+        src={"/images/user.svg"}
+        alt={"Search"}
+        width={24}
+        height={24}
+        priority={true}
+      />
+      <Image
+        src={"/images/bag.svg"}
+        alt={"Search"}
+        width={24}
+        height={24}
+        priority={true}
+      />
+    </div>
+  );
+};
+
+const categoriesMen = [
+  {
+    id: crypto.randomUUID(),
+    name: "Брюки и джинсы",
+    count: 1,
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Рубашки",
+    count: 1,
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Майки и футболки",
+    count: 1,
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Худи и свитеры",
+    count: 1,
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Пиджаки",
+    count: 1,
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Верхняя одежда",
+    count: 1,
+  },
+];
+const categoriesWomen = [
+  {
+    id: crypto.randomUUID(),
+    name: "Платья",
+    count: 2,
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Брюки и джинсы",
+    count: 2,
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Юбки",
+    count: 2,
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Свитеры",
+    count: 2,
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Жакеты",
+    count: 2,
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Топы и футболки",
+    count: 2,
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Рубашки",
+    count: 2,
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Верхняя одежда",
+    count: 2,
+  },
+];
+const categoriesAccessories = [
+  {
+    id: crypto.randomUUID(),
+    name: "Шапки",
+    count: 3,
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Шарфы",
+    count: 3,
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Капюшоны",
+    count: 3,
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Балаклавы",
+    count: 3,
+  },
+];
+
+const HeaderCategories: FC<{
+  categoriesType: "women" | "men" | "accessories";
+}> = ({ categoriesType }) => {
+  return (
+    <section
+      className={
+        "absolute flex min-h-81 w-full flex-col border-t-1 border-zinc-950 bg-[#ffffff]"
+      }
+    >
+      <div className={"flex min-h-11.5 items-center border-b-1"}>
+        <span
+          className={
+            "flex h-full w-[23%] items-center border-r-1 border-zinc-950 px-13.5 py-4"
+          }
+        >
+          {categoriesType !== "accessories" && "ОДЕЖДА"}
+        </span>
+        <span
+          className={
+            "flex h-full w-[23%] items-center border-r-1 border-zinc-950 px-4 py-4"
+          }
+        >
+          АКСЕССУАРЫ
+        </span>
+        <span></span>
+      </div>
+      <div className={"flex min-h-68"}>
+        <ul className={"w-[23%] border-r-1 border-zinc-950 px-13.5 py-4"}>
+          {categoriesType === "men" &&
+            categoriesMen.map((category) => (
+              <li
+                key={category.id}
+                className={
+                  "hover:text-hover active:text-active cursor-pointer transition-colors"
+                }
+              >
+                {category.name} ({category.count})
+              </li>
+            ))}
+          {categoriesType === "women" &&
+            categoriesWomen.map((category) => (
+              <li
+                key={category.id}
+                className={
+                  "hover:text-hover active:text-active cursor-pointer transition-colors"
+                }
+              >
+                {category.name} ({category.count})
+              </li>
+            ))}
+        </ul>
+        <ul className={"w-[23%] border-r-1 border-zinc-950 px-4 py-4"}>
+          {categoriesAccessories.map((category) => (
+            <li
+              key={category.id}
+              className={
+                "hover:text-hover active:text-active cursor-pointer transition-colors"
+              }
+            >
+              {category.name} ({category.count})
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className={"w-2/4"}>
+        <div className={"h-11.5"}></div>
+      </div>
+    </section>
   );
 };
