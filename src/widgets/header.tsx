9789095032eb5ad1a9a79da7c5ed7bc8f7e48775
Alexander Mitrofanov <www.lexter70@gmail.com>
2025-09-20 12:48:08 +0300

    enhance Header component with className prop and add navigation with logo and action buttons

--- src/widgets/header.tsx
+++ src/widgets/header.tsx
@@ -1,9 +1,12 @@
 import { FC, ReactNode } from "react";
 
-export const Header: FC<{ children: ReactNode }> = ({ children }) => {
+export const Header: FC<{ children: ReactNode; className?: string }> = ({
+  children,
+  className,
+}) => {
   return (
     <header>
-      <section className={"px-13.5 py-3.5"}>{children}</section>
+      <section className={`px-13.5 py-3.5 ${className}`}>{children}</section>
     </header>
   );
 };
